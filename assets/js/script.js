a80f531d6ca2ba33a858695b2fd2688744268e74
Dianne <dianne.diazm@gmail.com>
2020-09-26 22:05:20 -0300

    segundo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,46 +6,76 @@ class Propietario {
         this.telefono = telefono;
     }
     datosPropietario(){
-        return `El propietario se llama ${this.nombre}. Su dirección es ${this.direccion} y su teléfono es ${this.telefono}`;
+        return `<p>Nombre: ${this.nombre}. Su dirección es ${this.direccion} y su teléfono es ${this.telefono}`;
     }
 }
 
 // Hijo y padre
 class Animal extends Propietario {
-    constructor(nombre,direccion,telefono,nombreanimal,tipo){
+    constructor(nombre,direccion,telefono,nombreMascota,tipo){
         super(nombre,direccion,telefono);
-        this.nombreanimal = nombreanimal;
+        this.nombreMascota = nombreMascota;
         this.tipo = tipo;
     }
     datosAnimal(){
-        return `El animal se llama ${this.nombreanimal} y es el tipo ${this.tipo}`;
+        return `El animal se llama ${this.nombreMascota} y es el tipo ${this.tipo}`;
     }
 }
 
-// Hijo
+// Hijo Perro
 class Perro extends Animal {
-    constructor(nombre,direccion,telefono,nombreanimal,tipo,enfermedad){
-        super(nombre,direccion,telefono,nombreanimal,tipo);
-        this.enfermedad = enfermedad;
+    constructor(nombre,direccion,telefono,nombreMascota,tipo,enfermedad){
+        super(nombre,direccion,telefono,nombreMascota,tipo);
+        this._enfermedad = enfermedad;
+    }
+    get enfermedad(){
+        return this._enfermedad;
+    }
+    set enfermedad(nuevaEnfermedad){
+        this._enfermedad = nuevaEnfermedad;
     }
 }
 
-// Hijo
+// Hijo Gato
 class Gato extends Animal {
-    constructor(nombre,direccion,telefono,nombreanimal,tipo,enfermedad){
-        super(nombre,direccion,telefono,nombreanimal,tipo);
-        this.enfermedad = enfermedad;
+    constructor(nombre,direccion,telefono,nombreMascota,tipo,enfermedad){
+        super(nombre,direccion,telefono,nombreMascota,tipo);
+        this._enfermedad = enfermedad;
+    }
+    get enfermedad(){
+        return this._enfermedad;
+    }
+    set enfermedad(nuevaEnfermedad){
+        this._enfermedad = nuevaEnfermedad;
     }
 }
 
-// Hijo
+// Hijo Conejo
 class Conejo extends Animal {
-    constructor(nombre,direccion,telefono,nombreanimal,tipo,enfermedad){
-        super(nombre,direccion,telefono,nombreanimal,tipo);
-        this.enfermedad = enfermedad;
+    constructor(nombre,direccion,telefono,nombreMascota,tipo,enfermedad){
+        super(nombre,direccion,telefono,nombreMascota,tipo);
+        this._enfermedad = enfermedad;
     }
-   
+    get enfermedad(){
+        return this._enfermedad;
+    }
+    set enfermedad(nuevaEnfermedad){
+        this._enfermedad = nuevaEnfermedad;
+    }
+
 }
 
-var conejo1 = new Conejo('Dianne','Ñuñoa','9999','Canela','Perro','mañosa');
-console.log(conejo1);
+let formulario = document.querySelector('form');
+let instancias = (event) => {
+    event.preventDefault();
+    let propietario = document.getElementById('propietario').value;
+    let direccion = document.getElementById('direccion').value;
+    let telefono = document.getElementById('telefono').value;
+    let nombreMascota = document.getElementById('nombreMascota').value;
+    let tipo = document.getElementById('tipo').value;
+    let enfermedad = document.getElementById('enfermedad').value;
+    let mascota = new Perro(propietario,telefono,direccion,nombreMascota,tipo,enfermedad);
+    
+    document.getElementById('resultado').innerHTML = "Los datos del propietario son: " + mascota.datosPropietario() + ".<p> Los datos de la mascota son: <p>" + mascota.datosAnimal() + " y su enfermedad es: " + enfermedad;
+}
+formulario.addEventListener('submit', instancias);
